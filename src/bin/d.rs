42b727c7a9426d8d9142d12b84b81b1ee949e4b7
masa <masatoshi.sboard@gmail.com>
2020-07-31 00:19:13 +0900

    Done d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -4,5 +4,21 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        p: [usize; n]
     }
+    let mut count = 0;
+    let mut i = 0;
+    loop {
+        if i >= n {
+            break;
+        }
+        if p[i] == i + 1 {
+            count += 1;
+            i += 2;
+        } else {
+            i += 1;
+        }
+    }
+    println!("{}", count);
 }
